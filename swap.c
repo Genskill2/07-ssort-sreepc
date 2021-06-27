c31447fbb2f7604f23ed8720ac4a1348801bb777
sreepc <83125847+sreepc@users.noreply.github.com>
2021-06-27 23:54:05 +0530

    Update swap.c

---
+++ swap.c
@@ -0,0 +1,16 @@
+void swap_max(int arr[], int l, int n){
+int c;
+int b;
+int amax=arr[n];
+for(int i=n+1;i<l;i++){
+if(arr[i]>amax){
+amax=arr[i];
+c=i;}}
+b=arr[n];
+arr[n]=arr[c];
+arr[c]=b;
+}
+void ssort(int arr[], int l){
+for(int i=0;i<=l-1;i++){
+swap_max(arr,l,i);}
+}
